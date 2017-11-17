dc3b16d70ae42e9c24fb8c295b97d289b47c19fb
WinterSunnyJun <496900910@qq.com>
2017-11-17 20:10:58 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -21,4 +21,40 @@ $(function () {
         }
         $('.Iabout-list li').eq(IaNOW).show().siblings().hide();
     })
+//    首页荣誉资质
+    var IhNow = 0;
+    var IhLi = $('.Inew-honor-list ul li').length;
+    $('.Inew-honor-left').click(function () {
+        IhNow--;
+        if(IhNow < 0){
+            IhNow = IhLi - 1;
+        }
+        $('.Inew-honor-list ul li').eq(IhNow).show().siblings().hide();
+    })
+    $('.Inew-honor-right').click(function () {
+        IhNow++;
+        if(IhNow > IhLi - 1){
+            IhNow = 0;
+        }
+        $('.Inew-honor-list ul li').eq(IhNow).show().siblings().hide();
+    })
+//    关于我们荣誉资质
+    $('.honor-list ul').bxSlider({
+        minSlides:4,
+        maxSlides:4,
+        moveSlides:4,
+        slideWidth:300
+    });
+
+//    关于我们人才招聘
+    $('.recruit-list li').click(function () {
+        $(this).find('.recruit-hide').slideToggle();
+        if ($(this).find('.recruit-hide').show()){
+            $(this).addClass('active');
+        }else {
+            $(this).removeClass('active');
+        }
+
+    })
+
 })
