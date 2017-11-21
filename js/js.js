ea3b571afcee458f6d41fda9503532fc10affde3
WinterSunnyJun <496900910@qq.com>
2017-11-21 19:15:30 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -4,6 +4,17 @@
 $(function () {
 //    轮播
     $('.banner-btn').css("margin-left",-($('.banner-btn').width()/2));
+
+        $('.banner-pic').bxSlider({
+            widthSlide:500
+        });
+        $('.banner-wrapper .bx-wrapper .bx-prev').html('←');
+        $('.banner-wrapper .bx-wrapper .bx-next').html('→');
+
+        $('.banner-wrapper .bx-wrapper .bx-prev').click(function () {
+            console.log(0);
+        })
+
 //   首页关于我们
     var IaNOW = 0;
     var IaLi = $('.Iabout-list li').length;
@@ -39,38 +50,104 @@ $(function () {
         $('.Inew-honor-list ul li').eq(IhNow).show().siblings().hide();
     })
 //    关于我们荣誉资质
-    $('.honor-list ul').bxSlider({
-        minSlides:4,
-        maxSlides:4,
-        moveSlides:4,
-        slideWidth:300
-    });
+    if($(window).width() > 780){
+        $('.honor-list ul').bxSlider({
+            minSlides:4,
+            maxSlides:4,
+            moveSlides:4,
+        });
+    }else {
+        $('.honor-list ul').bxSlider({
+            minSlides:1,
+            maxSlides:1,
+            moveSlides:1,
+        });
+    }
+
 
 //    关于我们人才招聘
     $('.recruit-list li').click(function () {
-        $(this).find('.recruit-hide').slideToggle();
-        if ($(this).find('.recruit-hide').css('display','block')){
-            $(this).addClass('active');
-        }else {
-            $(this).removeClass('active');
-        }
-
-    })
-
+            $(this).addClass('active').siblings().removeClass('active');
+        })
 //    产品中心侧栏
-    $('.pd-sidebar-list > li').click(function () {
-        $(this).find('.pd-sublist').toggle(
-            function () {
-                $(this).find('.pd-sublist').show();
-                $(this).addClass('active');
-            },
-            function () {
-                $(this).find('.pd-sublist').hide();
-                $(this).removeClass('active');
-            }
-        )
+    $('.pd-sidebar-list > li').click(function (e) {
+        if($(this).find('.pd-sublist').css('display') == 'none'){
+            $(this).addClass('active').siblings().removeClass('active');
+            $(this).find('.pd-sublist').show();
+            $(this).siblings().children('ol.pd-sublist').hide();
+        }else{
+            $(this).removeClass('active');
+            $(this).find('.pd-sublist').hide();
+        }
     })
 
+//    案例详情
+    $('#zySlide').zySlide();
 
+// 产品详情
+    if($(window).width() > 768){
+        $('.pdxq-slide').bxSlider({
+            minSlides:3,
+            maxSlides:3,
+            moveSlides:1,
+            slideWidth: 400
+        });
+    }
+    $('.pdxq-recommend-list .bx-wrapper .bx-prev').html('&lt;')
+    $('.pdxq-recommend-list .bx-wrapper .bx-next').html('&gt;')
+
+    var pdLi = $('.pdxq-pic-btn ul li').length;
+    var pdPicH = $('.pdxq-pic-btn ul').height();
+    var pdPicM =$('.pdxq-pic-btn ul li').outerHeight() + 10;
+    var pdPici = 0;
+
+    $('.pdxq-pic-l').click(function () {
+        pdPici ++;
+        if(pdPici > pdLi - 1){
+            pdPici = 0;
+        }
+        $('.pdxq-pic-btn ul').css('margin-top',-pdPicM*pdPici + 'px');
+        console.log(pdLi);
+        console.log(pdPici);
+    })
+
+    $('.pdxq-pic-r').click(function () {
+        pdPici --;
+        if(pdPici < 0){
+            pdPici = pdLi - 1;
+        }
+        $('.pdxq-pic-btn ul').css('margin-top',-pdPicM*pdPici + 'px');
+
+    })
+
+    $('.pdxq-pic-btn ul li').click(function () {
+        var Inow = $(this).index();
+        $('.pdxq-pic-pic ul li').eq(Inow).show().siblings().hide();
+    })
+
+    var pdTXTh = $('.pdxq-text-box p').height();
+    var pdTXTm = $('.pdxq-text-box').height();
+    var pdTXTpage = Math.ceil(pdTXTh/pdTXTm);
+    var pdTXTi = 0;
+
+    $('.pd-text-l').click(function () {
+        pdTXTi--;
+        if(pdTXTi < 0){
+            pdTXTi = pdTXTpage -1;
+        }
+        $('.pdxq-text-box p').css('margin-top',-pdPicM*pdTXTi);
+    })
+    $('.pd-text-r').click(function () {
+        pdTXTi++;
+        if(pdTXTi > pdTXTpage -1){
+            pdTXTi = 0;
+        }
+        $('.pdxq-text-box p').css('margin-top',-pdPicM*pdTXTi);
+    })
+
+//    手机导航
+    $('#sj-nav-bar').click(function () {
+        $('.sj-nav-menu').slideToggle();
+    })
 
 })
